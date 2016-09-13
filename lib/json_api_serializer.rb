5f4a42fe2413569b648b11cd3624d44ab649e25f
Justin Giancola <justin.giancola@gmail.com>
2016-09-13 14:47:53 -0700

    Add support for serializing relationships

--- lib/json_api_serializer.rb
+++ lib/json_api_serializer.rb
@@ -1,6 +1,8 @@
 require "json_api_serializer/version"
 
 module JsonApiSerializer
+  Relationship = Struct.new(:name, :type, :options)
+
   class Model
     attr_reader :object
 
@@ -12,25 +14,35 @@ module JsonApiSerializer
       attrs.each do |attr|
         next if [ :id, :type ].include?(attr)
 
-        @attributes << attr
+        case attr
+        when /_id$/
+          resource_name = attr.to_s.sub(/_id$/, '').to_sym
+
+          add_relationship Relationship.new(resource_name, :has_one, {})
+        when /_ids$/
+          resource_name = attr.to_s.sub(/_ids$/, '').pluralize.to_sym
+
+          add_relationship Relationship.new(resource_name, :has_many, {})
+        else
+          @attributes << attr
+        end
       end
+    end
+
+    def self.relationships
+      @relationships || []
+    end
+
+    def self.has_one(resource_name, options={})
+      add_relationship Relationship.new(resource_name, :has_one, options)
+    end
+
+    def self.has_many(resource_name, options={})
+      add_relationship Relationship.new(resource_name, :has_many, options)
     end
 
     def initialize(object, options={})
       @object = object
-    end
-
-    def resource_object
-      resource_identifier_object.tap do |object|
-        object[:attributes] = attributes unless attributes.empty?
-      end
-    end
-
-    def attributes
-      @_attributes ||= self.class.attributes.inject({}) do |attrs, a|
-        attrs[a] = self.respond_to?(a) ? self.send(a) : object.send(a)
-        attrs
-      end
     end
 
     def resource_identifier_object
@@ -40,16 +52,65 @@ module JsonApiSerializer
       }
     end
 
+    def resource_object
+      resource_identifier_object.tap do |object|
+        object[:attributes] = attributes unless attributes.empty?
+        object[:relationships] = relationships unless relationships.empty?
+      end
+    end
+
     def as_json(*args)
       {
         data: resource_object
       }
     end
 
+    def attributes
+      @_attributes ||= self.class.attributes.inject({}) do |attrs, a|
+        attrs[a] = self.respond_to?(a) ? self.send(a) : object.send(a)
+        attrs
+      end
+    end
+
+    def relationships
+      @_relationships ||= self.class.relationships.inject({}) do |rels, rel|
+        if rel.options[:include]
+          # TODO: add corresponding payload to the included data
+        end
+
+        case rel.type
+        when :has_one
+          rel_fk = "#{rel.name}_id"
+          rel_id = object.send(rel_fk)
+          rel_type = rel.name.to_s.pluralize
+          rel_resource_identifier_object = { id: rel_id, type: rel_type }
+
+          rels[rel.name] = { data: rel_resource_identifier_object }
+        when :has_many
+          rel_fk = "#{rel.name.to_s.singularize}_ids"
+          rel_ids = object.send(rel_fk)
+          rel_type = rel.name.to_s.pluralize
+          rel_resource_identifier_objects = rel_ids.map do |rel_id|
+            { id: rel_id, type: rel_type }
+          end
+
+          rels[rel.name] = { data: rel_resource_identifier_objects }
+        end
+
+        rels
+      end
+    end
+
     private
 
     def resource_type
       object.class.name.downcase.pluralize
+    end
+
+    def self.add_relationship(relationship)
+      @relationships ||= []
+
+      @relationships << relationship
     end
   end
 end
--- spec/json_api_serializer_spec.rb
+++ spec/json_api_serializer_spec.rb
@@ -88,5 +88,155 @@ describe JsonApiSerializer do
         expect(serializer.as_json).to eq(expected_payload)
       end
     end
+
+    context "has_one relationships" do
+      class PostSerializerWithAuthor < JsonApiSerializer::Model
+        attributes :title, :body
+
+        has_one :author
+      end
+
+      let(:author) { Author.create!(name: 'fred', email: 'fred@flintstone.org') }
+
+      let(:post) { Post.create!(title: 'post title', body: 'post body', author: author) }
+
+      subject(:serializer) { PostSerializerWithAuthor.new(post) }
+
+      let(:expected_payload) do
+        {
+          data: {
+            type: "posts",
+            id: post.id,
+            attributes: {
+              title: post.title,
+              body: post.body
+            },
+            relationships: {
+              author: {
+                data: { id: author.id, type: "authors" }
+              }
+            }
+          }
+        }
+      end
+
+      it "generates a JSON-API-compliant payload" do
+        expect(serializer.as_json).to eq(expected_payload)
+      end
+    end
+
+    context "short-form has_one relationships" do
+      class PostSerializerWithShortAuthor < JsonApiSerializer::Model
+        attributes :title, :body, :author_id
+      end
+
+      let(:author) { Author.create!(name: 'fred', email: 'fred@flintstone.org') }
+
+      let(:post) { Post.create!(title: 'post title', body: 'post body', author: author) }
+
+      subject(:serializer) { PostSerializerWithShortAuthor.new(post) }
+
+      let(:expected_payload) do
+        {
+          data: {
+            type: "posts",
+            id: post.id,
+            attributes: {
+              title: post.title,
+              body: post.body
+            },
+            relationships: {
+              author: {
+                data: { id: author.id, type: "authors" }
+              }
+            }
+          }
+        }
+      end
+
+      it "generates a JSON-API-compliant payload" do
+        expect(serializer.as_json).to eq(expected_payload)
+      end
+    end
+
+    context "has_many relationships" do
+      class AuthorSerializerWithPosts < JsonApiSerializer::Model
+        attributes :name, :email
+
+        has_many :posts
+      end
+
+      let(:author) { Author.create!(name: 'fred', email: 'fred@flintstone.org') }
+
+      let!(:post1) { Post.create!(title: 'post1 title', body: 'post1 body', author: author) }
+      let!(:post2) { Post.create!(title: 'post2 title', body: 'post2 body', author: author) }
+
+      subject(:serializer) { AuthorSerializerWithPosts.new(author) }
+
+      let(:expected_payload) do
+        {
+          data: {
+            type: "authors",
+            id: author.id,
+            attributes: {
+              name: author.name,
+              email: author.email
+            },
+            relationships: {
+              posts: {
+                data: [
+                  { id: post1.id, type: "posts" },
+                  { id: post2.id, type: "posts" }
+                ]
+              }
+            }
+          }
+        }
+      end
+
+      it "generates a JSON-API-compliant payload" do
+        expect(serializer.as_json).to eq(expected_payload)
+      end
+    end
+
+    context "short-form has_many relationships" do
+      class AuthorSerializerWithShortPosts < JsonApiSerializer::Model
+        attributes :name, :email, :post_ids
+      end
+
+      let(:author) { Author.create!(name: 'fred', email: 'fred@flintstone.org') }
+
+      let!(:post1) { Post.create!(title: 'post1 title', body: 'post1 body', author: author) }
+      let!(:post2) { Post.create!(title: 'post2 title', body: 'post2 body', author: author) }
+
+      subject(:serializer) { AuthorSerializerWithShortPosts.new(author) }
+
+      let(:expected_payload) do
+        {
+          data: {
+            type: "authors",
+            id: author.id,
+            attributes: {
+              name: author.name,
+              email: author.email
+            },
+            relationships: {
+              posts: {
+                data: [
+                  { id: post1.id, type: "posts" },
+                  { id: post2.id, type: "posts" }
+                ]
+              }
+            }
+          }
+        }
+      end
+
+      it "generates a JSON-API-compliant payload" do
+        expect(serializer.as_json).to eq(expected_payload)
+      end
+    end
+
+    context "included relationships"
   end
 end
