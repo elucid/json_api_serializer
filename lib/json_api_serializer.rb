9ec9371b687c0188fafc1cdb7054afee1562e6fa
Justin Giancola <justin.giancola@gmail.com>
2016-09-13 13:25:37 -0700

    Add spec for simple model serializer, no relationships

--- lib/json_api_serializer.rb
+++ lib/json_api_serializer.rb
@@ -1,5 +1,55 @@
 require "json_api_serializer/version"
 
 module JsonApiSerializer
-  # Your code goes here...
+  class Model
+    attr_reader :object
+
+    def self.attributes(*attrs)
+      @attributes ||= []
+
+      return @attributes if attrs.empty?
+
+      attrs.each do |attr|
+        next if [ :id, :type ].include?(attr)
+
+        @attributes << attr
+      end
+    end
+
+    def initialize(object, options={})
+      @object = object
+    end
+
+    def resource_object
+      resource_identifier_object.tap do |object|
+        object[:attributes] = attributes unless attributes.empty?
+      end
+    end
+
+    def attributes
+      @_attributes ||= self.class.attributes.inject({}) do |attrs, a|
+        attrs[a] = self.respond_to?(a) ? self.send(a) : object.send(a)
+        attrs
+      end
+    end
+
+    def resource_identifier_object
+      {
+        id: object.id,
+        type: resource_type,
+      }
+    end
+
+    def as_json(*args)
+      {
+        data: resource_object
+      }
+    end
+
+    private
+
+    def resource_type
+      object.class.name.downcase.pluralize
+    end
+  end
 end
--- spec/json_api_serializer_spec.rb
+++ spec/json_api_serializer_spec.rb
@@ -9,4 +9,84 @@ describe JsonApiSerializer do
     expect{ Author }.to_not raise_exception
     expect{ Post }.to_not raise_exception
   end
+
+  describe JsonApiSerializer::Model do
+    context "resource identifier object default" do
+      let(:author) { Author.create!(name: 'fred', email: 'fred@flintstone.org') }
+
+      subject(:serializer) { JsonApiSerializer::Model.new(author) }
+
+      let(:expected_payload) do
+        {
+          data: {
+            type: "authors",
+            id: author.id
+          }
+        }
+      end
+
+      it "generates a JSON-API-compliant payload that includes just the resource identifier object" do
+        expect(serializer.as_json).to eq(expected_payload)
+      end
+    end
+
+    context "no relationships" do
+      class AuthorSerializer < JsonApiSerializer::Model
+        attributes :id, :name, :email
+      end
+
+      let(:author) { Author.create!(name: 'fred', email: 'fred@flintstone.org') }
+
+      subject(:serializer) { AuthorSerializer.new(author) }
+
+      let(:expected_payload) do
+        {
+          data: {
+            type: "authors",
+            id: author.id,
+            attributes: {
+              name: author.name,
+              email: author.email
+            }
+          }
+        }
+      end
+
+      it "generates a JSON-API-compliant payload" do
+        expect(serializer.as_json).to eq(expected_payload)
+      end
+    end
+
+    context "virtual attributes" do
+      class AuthorFigSerializer < JsonApiSerializer::Model
+        attributes :id, :name, :email, :figs
+
+        def figs
+          "JUICY"
+        end
+      end
+
+      let(:author) { Author.create!(name: 'fred', email: 'fred@flintstone.org') }
+
+      subject(:serializer) { AuthorFigSerializer.new(author) }
+
+      let(:expected_payload) do
+        {
+          data: {
+            type: "authors",
+            id: author.id,
+            attributes: {
+              name: author.name,
+              figs: "JUICY",
+              email: author.email
+            }
+          }
+        }
+      end
+
+      it "generates a JSON-API-compliant payload with virtual attributes" do
+        expect(serializer.as_json).to eq(expected_payload)
+      end
+    end
+  end
 end
